78883eb2f3158dfd9711a5bac7b07a0e310cf3f4
Guy Umbright <gumbright@gmail.com>
2013-05-16 18:58:57 -0500

    Initial Commit

---
+++ UIXCircularPagerDemo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  UIXCircularPagerDemo
+//
+//  Created by Guy Umbright on 5/16/13.
+//  Copyright (c) 2013 Umbright Consulting, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ UIXCircularPagerDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  UIXCircularPagerDemo
+//
+//  Created by Guy Umbright on 5/16/13.
+//  Copyright (c) 2013 Umbright Consulting, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
